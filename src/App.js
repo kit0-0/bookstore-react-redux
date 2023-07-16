414850fee6aaff4dc92e29f8892e31b42df66d53
kirti arora <111427307+kit0-0@users.noreply.github.com>
2023-07-16 17:58:08 +0000

    import router, booksContainer, navbar, categories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BooksContainer from './pages/BooksContainer';
+import Navbar from './components/Navbar';
+import Categories from './pages/Categories';
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<BooksContainer />} />
+            <Route path="/categories" element={<Categories />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
