79cf4c9e343fcd6f32bec1e519dbce347429fa3b
kit0-0 <kt1767@gmail.com>
2023-07-20 17:06:01 +0530

    Add addBook and removeBook actions

--- src/pages/BooksContainer.js
+++ src/pages/BooksContainer.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import AddBook from '../components/AddBook';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from '../components/Book';
+import AddBook from '../components/AddBook';
+import { addBook, removeBook } from '../redux/books/booksSlice';
 
-const BooksContainer = () => (
-  <Container className="bg-light m-3 p-3">
-    <div className="mb-3">
-      <Book title="The Hunger Games" author="Suzanne Collins" />
-    </div>
-    <div className="mb-3">
-      <Book title="Dune" author="Frank Herbert" />
-    </div>
-    <div className="mb-5">
-      <Book title="Capital in the Twenty-First Century" author="Suzanne Collins" />
-    </div>
-    <hr />
-    <AddBook />
-  </Container>
-);
+const BooksContainer = () => {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = (itemId) => {
+    dispatch(removeBook(itemId));
+  };
+
+  const handleAddBook = (newBook) => {
+    dispatch(addBook(newBook));
+  };
+
+  return (
+    <Container className="bg-light m-3 p-3">
+      {books.map((book) => (
+        <div className="mb-3" key={book.item_id}>
+          <Book
+            category={book.category}
+            title={book.title}
+            author={book.author}
+            onRemove={() => handleRemoveBook(book.item_id)}
+          />
+        </div>
+      ))}
+      <hr />
+      <AddBook onAdd={handleAddBook} />
+    </Container>
+  );
+};
 
 export default BooksContainer;
