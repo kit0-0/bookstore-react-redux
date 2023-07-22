f1a6b2874cca8aa80a3b08ea906f54dd276ac03d
kit0-0 <kt1767@gmail.com>
2023-07-22 17:02:01 +0530

    Add axios to dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
